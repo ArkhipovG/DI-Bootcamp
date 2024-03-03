0ab4df32404601eb604ad2bb447d49d90685ce40
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-03 13:46:44 +0200

    added Ex,4,5

--- Week1/Day1/ExercisesXP/Code.py
+++ Week1/Day1/ExercisesXP/Code.py
@@ -11,3 +11,17 @@ print(3 == 3) #True
 print(3 == "3") #False
 # print("3" > 3) #TypeError
 print("Hello" == "hello") #False
+
+#Exercise4
+computer_brand = "Apple"
+print(f"I have an {computer_brand} computer")
+
+#Exercise5
+name = "Gregory"
+age = 27
+shoe_size = 42
+info  = f"My name is {name} and I am {age} years old. My shoe size is {shoe_size}"
+print(info)
+
+
+
