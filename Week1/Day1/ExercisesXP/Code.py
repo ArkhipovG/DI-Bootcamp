78614de230095f21ff0ae565a9a901807e0c2334
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-03 13:38:07 +0200

    Ex1,2,3

---
+++ Week1/Day1/ExercisesXP/Code.py
@@ -0,0 +1,13 @@
+#Exercise1
+print("Hello World!\n" * 4)
+
+#Exercise2
+result = (99**3)*8
+print(result)
+
+#Exercise3
+print(5 < 3) #False
+print(3 == 3) #True
+print(3 == "3") #False
+# print("3" > 3) #TypeError
+print("Hello" == "hello") #False
