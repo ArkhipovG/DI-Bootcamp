137dd83674287716c663fd8b99de98cfdc2b7b7f
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-09 10:35:04 +0200

    Finished Challenges2 in Day 5

--- Week1/Day5/Challenges2/code.py
+++ Week1/Day5/Challenges2/code.py
@@ -46,3 +46,28 @@ def draw_doubled_right_triangle():
 
 draw_doubled_right_triangle()
 
+# Exercise 2
+
+my_list = [2, 24, 12, 354, 233]
+for i in range(len(my_list) - 1):
+    # Loop through each element in the list except the last one,
+    # i iterates from 0 to 3.
+
+    minimum = i
+    # Set the index of the minimum element to the current index.
+
+    for j in range( i + 1, len(my_list)):
+        # Loop through the remaining elements in the list starting from i + 1.
+
+        if(my_list[j] < my_list[minimum]):
+            # Compare the current element with the minimum element
+
+            minimum = j
+            # If the current element is smaller, update the index of the minimum element.
+
+            if(minimum != i):
+                my_list[i], my_list[minimum] = my_list[minimum], my_list[i]
+                # If the index of the minimum element is not the same as the current index,
+                # swap the elements at positions i and minimum.
+
+print(my_list)
