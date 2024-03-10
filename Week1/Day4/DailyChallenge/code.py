d18f627742f1804fcd5566cdda75818e51966952
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-10 23:22:07 +0200

    Found another way to solve this Matrix (weird one) from Daily Challenge in Day 4(Week1) the right code is in the bottom

--- Week1/Day4/DailyChallenge/code.py
+++ Week1/Day4/DailyChallenge/code.py
@@ -13,17 +13,49 @@ matrix = [list(row) for row in matrix_str.strip().split('\n')]
 
 transposed_matrix = list(zip(*matrix))
 
-decoded_message = ''
+encrypted_message = [''.join(i) for column in transposed_matrix for i in column if i.isalpha()]
+for i in range(len(encrypted_message)):
+    if encrypted_message[i] == 'i' and encrypted_message[i+1] == 's':
+        encrypted_message.insert(i+2, ' ')
+encrypted_message = "Neo, " + ''.join(encrypted_message)
+print(encrypted_message)
 
-for column in transposed_matrix:
-    word = ''
-    for char in column:
-        if char.isalpha():
-            word += char
-        elif word:
-            decoded_message += word + ' '
-            word = ''
-    if word:
-        decoded_message += word
 
-print(f"Neo, {decoded_message.strip()}")
+
+
+
+
+
+
+
+
+# matrix_str = """
+# 7ii
+# Tsx
+# h%?
+# i #
+# sM
+# $a
+# #t%
+# ^r!
+# """
+#
+# matrix = [list(row) for row in matrix_str.strip().split('\n')]
+#
+# transposed_matrix = list(zip(*matrix))
+# print(transposed_matrix)
+# decoded_message = ''
+#
+#
+# for column in transposed_matrix:
+#     word = ''
+#     for char in column:
+#         if char.isalpha():
+#             word += char
+#         elif word:
+#             decoded_message += word + ' '
+#             word = ''
+#     if word:
+#         decoded_message += word
+#
+# print(f"Neo, {decoded_message.strip()}")
