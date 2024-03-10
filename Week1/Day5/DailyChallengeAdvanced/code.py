193b3bf42f12a9719ddcabf893db2d7125c0a904
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-10 19:42:37 +0200

    added collecting pairs in list, counting unique and same copules in Advanced Daily challenge from Day 5 (week 1)

--- Week1/Day5/DailyChallengeAdvanced/code.py
+++ Week1/Day5/DailyChallengeAdvanced/code.py
@@ -5,14 +5,17 @@ list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 target_number = 3728
 
 compliment_list = []
-pair_count = 0
+unique_couples_dict = {}
+list_couples = []
 
 for num in list_of_numbers:
     compliment_list.append(target_number - num)
 
-
 for num in compliment_list:
     if num in list_of_numbers:
-        pair_count += 1
+        unique_couples_dict[num] = target_number - num
+        list_couples.append([num, target_number - num])
 
-print("Number of pairs summing to target_number:", pair_count)
+print("Number of pairs summing to target_number:", len(list_couples))
+print("Number of unique pairs:", len(unique_couples_dict))
+print("Number of same couples: ", len(list_couples) - len(unique_couples_dict))
