31bcf13f7a14d11c462dba285d49069af5d6d21d
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-11 22:05:42 +0200

    Changed method of creating list of pages Daily Challenge in Day 2 (Week2)

--- Week2/Day2/DailyChallenge/code.py
+++ Week2/Day2/DailyChallenge/code.py
@@ -2,16 +2,7 @@ class Pagination:
     def __init__(self, items=None, pageSize=10):
         self.items = items
         self.pageSize = pageSize
-        self.totalPages =[]
-        self.page = []
-        count = 0
-        for i, letter in enumerate(items):
-            self.page.append(letter)
-            count += 1
-            if count == self.pageSize or i == len(self.items) - 1:
-                self.totalPages.append(self.page)
-                self.page = []
-                count = 0
+        self.totalPages = [self.items[i:i + self.pageSize] for i in range(0, len(self.items), self.pageSize)]
         self.currentPage = self.totalPages[0]
 
     def getVisibleItems(self):
@@ -21,6 +12,8 @@ class Pagination:
         current_index = self.totalPages.index(self.currentPage)
         if self.currentPage != self.totalPages[0]:
             self.currentPage = self.totalPages[current_index - 1]
+        else:
+            print("It is the first page")
         return self
 
     def nextPage(self):
@@ -38,7 +31,6 @@ class Pagination:
             print("It is the last page")
         return self
 
-
     def firstPage(self):
         if self.currentPage != self.totalPages[0]:
             self.currentPage = self.totalPages[0]
@@ -47,7 +39,7 @@ class Pagination:
         return self
 
     def go_to_page(self, page):
-        if page > 0 and page < len(self.totalPages):
+        if 0 < page < len(self.totalPages):
             self.currentPage = self.totalPages[page - 1]
         elif page > len(self.totalPages):
             self.currentPage = self.totalPages[-1]
@@ -56,8 +48,6 @@ class Pagination:
         elif page == 0:
             self.currentPage = self.totalPages[0]
         return self
-
-
 
 
 alphabetList = list("abcdefghijklmnopqrstuvwxyz")
@@ -82,4 +72,3 @@ p.lastPage()
 p.prevPage().getVisibleItems()
 p.firstPage().getVisibleItems()
 p.go_to_page(3).getVisibleItems()
-
