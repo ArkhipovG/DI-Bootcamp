28f29e688c5b3054ba5b9032bdd838c491f112d8
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-15 09:13:54 +0200

    Finished Mini Project Rock Paper Scissors in Day 5 (Week2)

---
+++ Week2/Day5/RockPaperScissors/game.py
@@ -0,0 +1,83 @@
+import random
+
+
+class Game:
+    def __init__(self):
+        self.game_mode = "vs computer"
+
+    @staticmethod
+    def get_user_item():
+        items = ['r', 'p', 's']
+        is_valid = False
+        while not is_valid:
+            user_item = input('Select (r)rock, (p)paper, (s)scissors): ')
+            if user_item in items:
+                is_valid = True
+                return user_item
+            else:
+                print('Invalid select')
+
+    @staticmethod
+    def get_computer_item():
+        items = ['r', 'p', 's']
+        computer_item = random.choice(items)
+        return computer_item
+
+    @staticmethod
+    def get_game_result(user_item, computer_item):
+
+        if user_item == 'p' and computer_item == 'r':
+            result = 1
+            return result
+        elif user_item == 'r' and computer_item == 's':
+            result = 1
+            return result
+        elif user_item == 's' and computer_item == 'p':
+            result = 1
+            return result
+        elif user_item == 'r' and computer_item == 'p':
+            result = 2
+            return result
+        elif user_item == 'p' and computer_item == 's':
+            result = 2
+            return result
+        elif user_item == 's' and computer_item == 'r':
+            result = 2
+            return result
+        elif user_item == computer_item:
+            result = 3
+            return result
+        else:
+            print('Unknown Error')
+
+    def play(self):
+        user_item = self.get_user_item()
+        computer_item = self.get_computer_item()
+        game_result = self.get_game_result(user_item, computer_item)
+                        
+        if game_result == 1:
+            print(
+                f"You choose {user_item}, computer choose {computer_item}, Result: You won!")
+        elif game_result == 2:
+            print(
+                f"You choose {user_item}, computer choose {computer_item}, Result: You lost.")
+        elif game_result == 3:
+            print(
+                f"You choose {user_item}, computer choose {computer_item}, Result: draw")
+
+        return game_result
+
+# def print_results(results):
+#     scores = {'win': 0 ,'loss': 0 ,'draw': 0}
+#     if results['result'] == 1:
+#         print(f"You choose {results['user_item']}, computer choose {results['computer_item']}, Result: You won!")
+#         scores['win'] += 1
+#     if results['result'] == 2:
+#         print(f"You choose {results['user_item']}, computer choose {results['computer_item']}, Result: You lost.")
+#         scores['loss'] += 1
+#     if results['result'] == 3:
+#         print(f"You choose {results['user_item']}, computer choose {results['computer_item']}, Result: draw")
+#         scores['draw'] += 1
+#     print(f'You won: {scores['win']} times')
+#     print(f'You lost: {scores['loss']} times')
+#     print(f'You drew: {scores['draw']} times')
---
+++ Week2/Day5/RockPaperScissors/rock-paper-scissors.py
@@ -0,0 +1,43 @@
+from game import Game
+
+
+def get_user_menu_choice():
+    user_choices = ['g', 'q']
+    print("(g)Play a new game")
+    print("(q)Quit")
+
+    get_user_choice = input(": ")
+    if get_user_choice in user_choices:
+        return get_user_choice
+    else:
+        print("Invalid choice")
+
+
+def print_results(scores):
+    print(f'You won: {scores['win']} times')
+    print(f'You lost: {scores['loss']} times')
+    print(f'You drew: {scores['draw']} times')
+
+
+def main():
+    print("Welcome to Rock Paper Scissors game!")
+    scores = {'win': 0, 'loss': 0, 'draw': 0}
+    end = False
+    while not end:
+        user_choice = get_user_menu_choice()
+        if user_choice == 'g':
+            game = Game()
+            result = game.play()
+            if result == 1:
+                scores['win'] += 1
+            elif result == 2:
+                scores['loss'] += 1
+            elif result == 3:
+                scores['draw'] += 1
+        if user_choice == 'q':
+            print_results(scores)
+            print("Goodbye!")
+            end = True
+
+
+main()
