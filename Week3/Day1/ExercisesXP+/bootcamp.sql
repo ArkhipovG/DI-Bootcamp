25b6d29cb0b6154ae0c34577f4afdff4b1a808c4
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2024-03-17 15:04:45 +0200

    Finished ExercisesXP+ in Day1 (Week3)

---
+++ Week3/Day1/ExercisesXP+/bootcamp.sql
@@ -0,0 +1,40 @@
+-- create table students
+--         (
+-- id SERIAL PRIMARY KEY,
+-- last_name VARCHAR (100) NOT NULL,
+-- first_name VARCHAR (50) NOT NULL,
+-- birth_date DATE NOT NULL
+--         );
+
+-- insert into students(last_name, first_name, birth_date)
+-- values ('Benichou','Marc','11/02/1998'), ('Cohen','Yoan','12/03/2010'), ('Benichou','Lea','07/27/1987'), ('Dux','Amelia','04/07/1996'), ('Grez','David','06/14/2003'), ('Simpson','Omer','10/03/1980');
+
+-- insert into students(last_name, first_name, birth_date)
+-- values ('Arkhipov','Gregory','10/16/1996');
+
+-- SELECT * from students;
+--
+-- SELECT first_name, last_name from students;
+
+-- SELECT first_name, last_name from students WHERE id=2;
+
+-- SELECT first_name, last_name from students WHERE last_name = 'Benichou' AND first_name = 'Marc';
+
+-- SELECT first_name, last_name from students WHERE last_name = 'Benichou' OR first_name = 'Marc';
+
+-- SELECT first_name, last_name from students WHERE first_name ILIKE'%a%';
+
+-- SELECT first_name, last_name from students WHERE first_name ILIKE'A%';
+
+-- SELECT first_name, last_name from students WHERE first_name ILIKE'%a';
+
+-- SELECT first_name, last_name from students WHERE first_name ILIKE'_%a%';
+
+-- SELECT first_name, last_name from students WHERE first_name ILIKE'%a';
+
+-- SELECT first_name, last_name from students WHERE id = 1 or id = 3;
+
+-- SELECT id, first_name, last_name from students WHERE birth_date >= '01/01/2000';
+
+
+
